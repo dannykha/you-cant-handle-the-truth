54f8cf92e8bb69dfa8c5e1c275bfd04d7ee1810b
Danny Kha <danny.n.kha@gmail.com>
2020-11-18 23:33:16 -0800

    commit 2, maybe this is the right return lolol

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -3,7 +3,10 @@ import org.junit.Test;
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
+        String[][] test = {
+                {"you know you love me i know you care just shout whenever and i will be there"},
+                {"baby baby baby oh like baby baby baby oh"},
+        };
 
-        HandleTruth.wordCount("testing testing testing okay works works maybe or not or yes");
     }
 }
